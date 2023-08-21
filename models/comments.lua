64d24c8bcb7fba98fbb0c094d117daaa673bb074
Alexis <thrifthshopdowntheroad@gmail.com>
2023-08-20 23:42:16 -0400

    :recycle: ordered comments by newest

--- models/comments.lua
+++ models/comments.lua
@@ -2,7 +2,7 @@ local Model = require("lapis.db.model").Model
 local Comment = Model:extend("comments")
 
 function Comment:GetCommentsByWaveId(wave_id)
-    local comments = Comment:select("where wave_id = ?", wave_id)
+    local comments = Comment:select("where wave_id = ? order by id desc ", wave_id)
     return comments
 end
 
