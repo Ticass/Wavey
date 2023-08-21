378ba6139290d309a97e651522a9f30067662d51
Alexis <thrifthshopdowntheroad@gmail.com>
2023-08-21 12:12:51 -0400

    :recycle: temporary URL change

--- aquascape/src/constants/urls.js
+++ aquascape/src/constants/urls.js
@@ -1,6 +1,6 @@
 const urls = {
     apiLocal: 'http://localhost:8080',
-    apiNgrok: 'https://93ba-70-31-217-181.ngrok-free.app/'
+    apiNgrok: 'https://93ba-70-31-217-181.ngrok-free.app'
 }
 
 export default urls
